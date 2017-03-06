b1fb469c505ffb999aad1f13f3fc5fabece93ad3
manoj <manoj@DESKTOP-FOO2V86.attlocal.net>
2017-03-05 20:22:54 -0600

    Signed-off-by: manoj <manoj@DESKTOP-FOO2V86.attlocal.net>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module' 
 
 import { AppComponent } from './app.component';
@@ -24,7 +23,6 @@ import { CommonService } from './common.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule,
     AppRoutingModule
   ],
   providers: [ PersonsService, PersonDetailsService, CommonService ],
